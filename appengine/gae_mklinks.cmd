35a1289d2d1ee23a677e0c15deb36484c5a3da33
the-happy-hippo <the-happy-hippo@users.noreply.github.com>
2014-03-21 10:40:59 +0200

    Small script fix

--- appengine/gae_mklinks.cmd
+++ appengine/gae_mklinks.cmd
@@ -1,13 +1,21 @@
 @echo off
 setlocal
 
-pushd %~dp0\lib
+set LIB_LINKS=%~dp0\lib
+
+if NOT EXIST "%LIB_LINKS%" md "%LIB_LINKS%"
+
+pushd "%LIB_LINKS%\..\.."
+
+set LIB_TARGET=%CD%
+
+cd "%LIB_LINKS%"
 
 FOR %%p IN (chardet cssselect readability codepen) DO (
   IF EXIST "%%p" (
     echo Existing lib: %%p
   ) ELSE (
     echo|set /p="Linking: %%p ... "
-    mklink /d %%p ..\..\%%p
+    mklink /d %%p "%LIB_TARGET%\%%p"
   )
 )
