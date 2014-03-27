6a74d3f914832a91f433ad89e21ed9113bc13d50
the-happy-hippo <th3hh0@gmail.com>
2014-03-27 21:53:35 +0200

    Add more goodies
    Client side:
    - Parameterize asset location and API tokens via Jade config in settings.json;
    - Add meta keywords and remove special chars (integrated from gh-pages);
    - Add loading progress indicator;
    - Add option to cleanup local storage;
    - Add option to input partial URLs (no 'http://');
    - Add alternative parsing API method.
    
    Server side:
      - Parameterize token in the server app.yaml settings;
      - Allow CORS from anywhere (guarded by token);
      - Fix server encoding exception.

--- appengine/spritsit.py
+++ appengine/spritsit.py
@@ -1,34 +1,40 @@
-
-import fixpath
+import os
+import re
+import json
 
 import webapp2
 import urllib
 import logging
+
+import fixpath
+
 from readability import readability
 from readability.readability import Document
 
-import lxml.html
-import re
-import json
+import lxml.html as lhtml
 
-MAGIC = '112358'
+# Dumb authorization for now
+READ_API_TOKEN = os.environ['READ_API_TOKEN']
 
 class BaseHandler(webapp2.RequestHandler):
 
     def _create_document(self):
 
-        if self.request.get('m') != MAGIC:
-            logging.exception('Magic is not valid')
+        req_token = self.request.get('token')
+
+        if req_token != READ_API_TOKEN:
+            logging.error('TOKEN field is not valid: \'{}\' is not {}',
+                req_token, READ_API_TOKEN)
             webapp2.abort(403) # forbidden
 
-        uri = self.request.get('u')
+        url = self.request.get('url')
 
-        if not uri:
-            logging.exception('URI parameter is not found in request')
+        if not url:
+            logging.error('URL parameter is not found in request')
             webapp2.abort(400) # bad request
 
         # Read raw html
-        rawhtml = urllib.urlopen(uri).read()
+        rawhtml = urllib.urlopen(url).read()
 
         # Parse with readability
         doc = Document(rawhtml)
@@ -37,13 +43,18 @@ class BaseHandler(webapp2.RequestHandler):
         html = doc.summary()
 
         # Reformat as plain text
-        txt = lxml.html.tostring(lxml.html.fromstring(html), method='text', encoding='utf-8')
+        txt = lhtml.tostring(lhtml.fromstring(html), method='text', encoding='utf-8')
+
         txt = txt.strip()
         txt = re.sub(r'\s+', r' ', txt, flags=re.MULTILINE)
         txt = re.sub(r'\)\.', r'). ', txt, flags=re.MULTILINE)
 
         # Create JSON object
-        return dict(uri=uri, title=doc.short_title(), content=txt)
+        return {
+            'url'       : url,
+            'title'     : doc.short_title(),
+            'content'   : txt,
+        }
 
 
 class TextHandler(BaseHandler):
@@ -52,13 +63,22 @@ class TextHandler(BaseHandler):
 
         doc = self._create_document()
 
-        self.response.write(
-            '{title}\n\n{uri}\n\n{content}\n\n'.format(**doc))
+        for field in ['title', 'content']: # 'url'
+            value = doc[field]
+
+            logging.debug('Writing field {} of {}'.format(
+                field, type(value)))
+
+            self.response.write(value)
+
+            self.response.write('\n\n')
 
 class JsonHandler(BaseHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'application/json'
-        self.response.headers['Access-Control-Allow-Origin'] = 'http://s.codepen.io'
+
+        # FIXME: Be more specific in CORS; but for now rely on 'token'
+        self.response.headers['Access-Control-Allow-Origin'] = '*'
 
         json.dump(self._create_document(), self.response)
 
